00ac8b590204044fed61f8cc029643fd1a8ebd7b
ashutosh-awasthi23 <112499427+ashutosh-awasthi23@users.noreply.github.com>
2024-09-04 19:56:22 +0530

    #

---
+++ LabsAssignment/LA_Calender.py
@@ -0,0 +1,4 @@
+import calendar
+yy=int(input("Enter the year:"))
+mm=int(input("Enter the month number :"))
+print(calendar.month(yy,mm))
---
+++ LabsAssignment/LA_LargestSmallest.py
@@ -0,0 +1,3 @@
+li=[int(x) for x in input("Enter the list").split()]
+print(f" The largest number in the string is {max(li)}")
+print(f"The smallest number in the string is {min(li)}")
